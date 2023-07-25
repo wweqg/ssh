2bee8fa38e1d291995dee0c0526a4db00ef5c678
wweqg <e0726629@u.nus.edu>
2023-07-26 01:05:48 +0800

    1

--- open3d_normal_test.py
+++ open3d_normal_test.py
@@ -3,8 +3,8 @@ import open3d as o3d
 # Load the point cloud from .ply file
 pcd = o3d.io.read_point_cloud("longdress_vox10_1051.ply")
 
-# Compute normals for the point cloud
-o3d.geometry.estimate_normals(pcd, search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.1, max_nn=30))
+# Estimate normals for the point cloud
+pcd.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.1, max_nn=30))
 
 # Save the point cloud with normals to a new .ply file
-o3d.io.write_point_cloud("out", pcd)
+o3d.io.write_point_cloud("out.ply", pcd)
